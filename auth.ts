10fde1a77c3051c432a1876460b595e48a8db8ef
kyher <kyle.heron@pm.me>
2025-10-22 17:26:46 +0100

    Add drizzle  and working auth in DB

--- auth.ts
+++ auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
+import { DrizzleAdapter } from "@auth/drizzle-adapter"
+import { db } from "./src/db/schema"
 import GitHub from "next-auth/providers/github"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [GitHub],
 })
