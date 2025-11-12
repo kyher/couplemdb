b935d85213ff79e01d90a21294ba7590e6b360d8
kyher <kyle.heron@pm.me>
2025-11-12 16:43:42 +0000

    Update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎬</text></svg>"
+      ></link>
       <body className="bg-gray-900 text-white min-h-screen">
         <main className="flex flex-col gap-4 font-mono p-6">{children}</main>
       </body>
